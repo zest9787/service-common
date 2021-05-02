16863637fa709b4452f26815850526d8056feeb5
한상현 <lucidman.han@gmail.com>
2021-05-03 00:56:08 +0900

    프로젝트 생성 1. springboot 프로젝트 생성

---
+++ src/test/java/kr/lucidman/service/common/ServiceCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.lucidman.service.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
